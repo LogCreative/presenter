8493a2f20c347487d30efc1569d4067f0a041cf5
LogCreative <logcreative@outlook.com>
2023-06-26 12:54:08 +0800

    docs: display test pdf in the doc

--- build.lua
+++ build.lua
@@ -8,3 +8,13 @@ typesetfiles     = {"presenter.tex"}
 sourcefiledir    = "source"
 
 testfiledir      = "testfiles"
+
+maindir          = "."
+builddir         = maindir .. "/build"
+testdir          = builddir .. "/test"
+typesetdir       = builddir .. "/doc"
+-- prepare the test pdf in the doc
+function docinit_hook()
+    cp("*.pdf", testdir, typesetdir)
+    return 0
+end
