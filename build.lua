07ecd9fea84f1375e89d22f24cb569f4d1958c27
LogCreative <logcreative@outlook.com>
2023-07-06 13:22:01 +0800

    fix: tag version

--- build.lua
+++ build.lua
@@ -23,13 +23,13 @@ end
 tagfiles         = {"*.dtx","presenter.tex"}
 function update_tag(file, content, tagname, tagdate)
     local iso = "%d%d%d%d%-%d%d%-%d%d"
-    local ver = "v%d.%d.%d"
+    local ver = "%d.%d.%d"
     if string.match(file, "%.dtx$") then
         return string.gsub(content, "{" .. iso .. "}{" .. ver .. "}", 
         "{" .. tagdate .. "}{" .. tagname .. "}")
     elseif string.match(file, "%.tex$") then
-        return string.gsub(content, "\\date{" .. iso .. " \\quad " .. ver .. "}",
-        "\\date{" .. tagdate .. " \\quad " .. tagname .. "}")
+        return string.gsub(content, "\\date{" .. iso .. " \\quad v" .. ver .. "}",
+        "\\date{" .. tagdate .. " \\quad v" .. tagname .. "}")
     end
     return content
 end
